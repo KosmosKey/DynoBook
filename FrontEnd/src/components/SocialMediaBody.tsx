eee04ff87035174bfe505b35feb0e0f1e24f67c1
KosmosKey <khamidovmaga2002@gmail.com>
2020-12-02 21:53:50 +0100

    Made skeleton for the profile loading...

--- FrontEnd/src/components/SocialMediaBody.tsx
+++ FrontEnd/src/components/SocialMediaBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { Picker } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
 import { useSelector } from "react-redux";
@@ -17,6 +17,7 @@ import OfflineUsers from "./ActiveUsers/OfflineUsers";
 import { userInformation } from "../reducerSlices/authSlicer";
 import CreateIcon from "@material-ui/icons/Create";
 import db from "./firebase";
+import { Skeleton } from "@material-ui/lab";
 
 const SocialMediaBody: React.FC = () => {
   const user = useSelector(userInformation);
@@ -28,6 +29,8 @@ const SocialMediaBody: React.FC = () => {
   const [textValue, setTextValue] = useState<string>("");
 
   const [emojiDisplay, setEmojiDisplay] = useState<boolean>(false);
+
+  const [profileLoading, setProfileLoading] = useState<boolean>(true);
 
   useEffect(() => {
     if (user) {
@@ -52,6 +55,8 @@ const SocialMediaBody: React.FC = () => {
           setPosts(snapshot.docs.map((doc) => doc.data()));
         });
     }
+
+    setProfileLoading(false);
   }, [user]);
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -128,7 +133,111 @@ const SocialMediaBody: React.FC = () => {
             <h1>DynoBook</h1>
           </div>
           <div className="SocialMediaBodyProfile__ProfileInformation">
-            <div className="SocialMediaProfile__Info">
+            {profileLoading ? (
+              <Fragment>
+                <div className="SocialMediaProfile__Info">
+                  <div className="AvatarProfile skeleton">
+                    <div className="Avatar__Div">
+                      <Skeleton
+                        variant="circle"
+                        animation="wave"
+                        className="Avatar__Skeleton"
+                      />
+                    </div>
+                    <div className="Name">
+                      <div className="Name__H1_Icon">
+                        <Skeleton
+                          animation="wave"
+                          className="SocialMediaBody__Skeleton"
+                        />
+                      </div>
+                      <Skeleton
+                        animation="wave"
+                        className="SocialMediaBody__Skeleton"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="SocialMediaProfile__Followers loading">
+                  <div className="Posts">
+                    <Skeleton
+                      animation="wave"
+                      className="SocialMediaBody__Skeleton"
+                    />
+                    <Skeleton
+                      animation="wave"
+                      className="SocialMediaBody__Skeleton"
+                    />
+                  </div>
+                  <div className="Posts">
+                    <Skeleton
+                      animation="wave"
+                      className="SocialMediaBody__Skeleton"
+                    />
+                    <Skeleton
+                      animation="wave"
+                      className="SocialMediaBody__Skeleton"
+                    />
+                  </div>
+                  <div className="Posts">
+                    <Skeleton
+                      animation="wave"
+                      className="SocialMediaBody__Skeleton"
+                    />
+                    <Skeleton
+                      animation="wave"
+                      className="SocialMediaBody__Skeleton"
+                    />
+                  </div>
+                </div>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <div className="SocialMediaProfile__Info">
+                  <div className="AvatarProfile">
+                    <div className="Avatar__Div">
+                      <Avatar
+                        src={user?.image && user.image}
+                        className="Avatar"
+                      >
+                        {`${!user?.image && user?.username?.charAt(0)}`}
+                      </Avatar>
+                      <div className="Avatar__Edit">
+                        <p>
+                          <CreateIcon />
+                          Edit
+                        </p>
+                      </div>
+                    </div>
+                    <div className="Name">
+                      <div className="Name__H1_Icon">
+                        <h1>
+                          {user?.first_name} {user?.last_name?.charAt(0)}.
+                        </h1>
+                        <CheckCircleIcon />
+                      </div>
+                      <p>@{user?.username}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="SocialMediaProfile__Followers">
+                  <div className="Posts">
+                    <h4>POSTS</h4>
+                    <h2>{posts?.length}</h2>
+                  </div>
+                  <div className="Posts">
+                    <h4>FOLLOWING</h4>
+                    <h2>{following?.length}</h2>
+                  </div>
+                  <div className="Posts">
+                    <h4>FOLLOWERS</h4>
+                    <h2>{followers?.length}</h2>
+                  </div>
+                </div>
+              </Fragment>
+            )}
+
+            {/* <div className="SocialMediaProfile__Info">
               <div className="AvatarProfile">
                 <div className="Avatar__Div">
                   <Avatar src={user?.image && user.image} className="Avatar">
@@ -165,7 +274,7 @@ const SocialMediaBody: React.FC = () => {
                 <h4>FOLLOWERS</h4>
                 <h2>{followers?.length}</h2>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
         <div className="SocialMediaBody__Suggestions">
